10fdb2e266fb0e52344f6f186403a89e388b3508
Nicholas LoBue <nlobue@aureusox.com>
2025-01-06 09:09:40 -0500

    Including a python virtual env

--- scripts/flare/claim_entity_rewards.py
+++ scripts/flare/claim_entity_rewards.py
@@ -1,7 +1,7 @@
 import json
 
 from web3 import Web3
-from web3.middleware import geth_poa_middleware
+from web3.middleware import ExtraDataToPOAMiddleware
 
 def wei_to_flr(gwei_amount):
     return gwei_amount / 1e18
@@ -9,7 +9,7 @@ def wei_to_flr(gwei_amount):
 if __name__ == '__main__':
     rpc = 'https://flare.aureusox.com/ext/bc/C/rpc'
     web3 = Web3(Web3.HTTPProvider(rpc))
-    web3.middleware_onion.inject(geth_poa_middleware, layer=0)
+    web3.middleware_onion.inject(ExtraDataToPOAMiddleware, layer=0)
 
     file_for_key = '../../.keys/ftso_flare_v2_claim_executor'
     password = input('Password for executor:')
@@ -26,7 +26,7 @@ if __name__ == '__main__':
     print(executor_account.address)
 
     reward_epochs_to_claim = list(map(int, input('Reward epochs to claim (comma separated): ').replace(" ", "").split(',')))
-    recipient = web3.toChecksumAddress(input('Where do you want to send rewards?: '))
+    recipient = web3.to_checksum_address(input('Where do you want to send rewards?: '))
     wrap = input('Do you want to wrap rewards? [y/n]: ')
     if wrap == 'y':
         wrap = True
@@ -37,11 +37,11 @@ if __name__ == '__main__':
         exit()
         
 
-    reward_manager_contract_address = web3.toChecksumAddress('0xC8f55c5aA2C752eE285Bd872855C749f4ee6239B')
+    reward_manager_contract_address = web3.to_checksum_address('0xC8f55c5aA2C752eE285Bd872855C749f4ee6239B')
     reward_manager_contract = web3.eth.contract(address=reward_manager_contract_address,
                                                         abi=reward_manager_abi)
 
-    entity_address = web3.toChecksumAddress('0xE3b7968c1B706461A8f63540080ECF4Ce70C30c0')
+    entity_address = web3.to_checksum_address('0xE3b7968c1B706461A8f63540080ECF4Ce70C30c0')
 
     # Build Proofs from Json's
     tx = {}
@@ -55,10 +55,10 @@ if __name__ == '__main__':
             data = json.load(json_file)
 
         for reward_claim in data['rewardClaims']:
-            if web3.toChecksumAddress(reward_claim['body']['beneficiary'])== entity_address:
-                merkle_proof = [web3.toBytes(hexstr=p) for p in reward_claim['merkleProof']]
+            if web3.to_checksum_address(reward_claim['body']['beneficiary'])== entity_address:
+                merkle_proof = [web3.to_bytes(hexstr=p) for p in reward_claim['merkleProof']]
                 reward_epoch_id = reward_claim['body']['rewardEpochId']
-                beneficiary = web3.toBytes(hexstr=reward_claim['body']['beneficiary'])[:20]
+                beneficiary = web3.to_bytes(hexstr=reward_claim['body']['beneficiary'])[:20]
                 claim_type = reward_claim['body']['claimType']
                 amount = int(reward_claim['body']['amount'])
 
@@ -78,7 +78,7 @@ if __name__ == '__main__':
     total_rewards = round(wei_to_flr(sum(total_rewards)), 4)
     print(f'\n\n ---- TX Details ---- \n reward_owner: {entity_address} \n recipient: {recipient} \n reward_epoch: {reward_epoch} ({reward_epochs_to_claim}) \n wrap: {wrap} \n proofs: {proofs} \n total rewards: {total_rewards}')
 
-    tx = reward_manager_contract.functions.claim(entity_address, recipient, reward_epoch, wrap, proofs).buildTransaction({
+    tx = reward_manager_contract.functions.claim(entity_address, recipient, reward_epoch, wrap, proofs).build_transaction({
         'from': executor_account.address,
         'nonce': web3.eth.get_transaction_count(executor_account.address)
     })
@@ -86,7 +86,7 @@ if __name__ == '__main__':
     # confirm before sending
     confirm = str(input(f'\nConfirm\n[y,n]:  '))
     if confirm == 'y':
-        signed_tx = executor_account.signTransaction(tx)
-        tx_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
+        signed_tx = executor_account.sign_transaction(tx)
+        tx_hash = web3.eth.send_raw_transaction(signed_tx.raw_transaction)
 
         print(tx_hash.hex())
